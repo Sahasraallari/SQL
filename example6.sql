d295cc398f471f711baa9e4e37fb65f7ec1729d2
Sahasraallari <mohan.velagala@gmail.com>
2026-03-13 17:40:24 +0530

    Salesman

---
+++ example6.sql
@@ -0,0 +1,17 @@
+CREATE TABLE  IF NOT EXISTS  Salesman(
+    Salesman_id TEXT PRIMARY KEY,
+    name TEXT,
+    city TEXT,
+    Commission REAL
+);
+
+INSERT INTO Salesman(Salesman_id, city, name, Commission)VALUES
+('5001', 'James Hoog', 'New York', 0.15),
+('5002', 'Nail Knite', 'Paris', 0.13),
+('5005', 'Pit Alex', 'London', 0.11),
+('5006', 'Mc Lyon', 'Paris', 0.14),
+('5007', 'Paul adam', 'Rome', 0.13),
+('5001', 'Lauson Hen', 'San Jose', 0.12);
+
+
+SELECT * FROM Salesman;
